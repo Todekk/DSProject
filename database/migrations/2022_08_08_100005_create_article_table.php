427c7b4a1d674f4e9fe30afd0c7141507baeeb1b
Teodor Todorov <teditods13@gmail.com>
2022-08-08 13:55:16 +0300

    DataBase connection established
    -Using MySQL Workbench

---
+++ database/migrations/2022_08_08_100005_create_article_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();            
+            $table->string('articleName');
+            $table->string('description');
+            $table->integer('price');
+            $table->string('image')->nullable();
+            $table->integer('user_id')->unsigned()->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+};
